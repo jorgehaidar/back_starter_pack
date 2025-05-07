52876fd5ac458db37f1a42d93b7a6e1fe71bd778
jorgehaidar <jorgehaidarmtnez@gmail.com>
2025-05-07 19:30:02 -0400

    feat[rule]: agregada la rule StrongPassword

--- src/Rules/StrongPassword.php
+++ src/Rules/StrongPassword.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Mbox\BackCore\Rules;
+namespace App\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
@@ -15,25 +15,17 @@ class StrongPassword implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $messages = [
-            'uppercase' => 'Must contain at least one uppercase letter.',
-            'lowercase' => 'Must contain at least one lowercase letter.',
-            'number' => 'Must contain at least one number.',
-            'symbol' => 'Must contain at least one symbol.',
-            'length' => 'Must be at least 8 characters long.',
-        ];
-
         $passes = [
-            'uppercase' => preg_match('/[A-Z]/', $value),
-            'lowercase' => preg_match('/[a-z]/', $value),
-            'number' => preg_match('/[0-9]/', $value),
-            'symbol' => preg_match('/[\W_]/', $value),
+            'letters' => '/[a-zA-Z]/',
+            'mixed' => preg_match('/(?=.*[a-z])(?=.*[A-Z])/', $value),
+            'numbers' => preg_match('/[0-9]/', $value),
+            'symbols' => preg_match('/[\W_]/', $value),
             'length' => strlen($value) >= 8,
         ];
 
         foreach ($passes as $key => $pass) {
             if (!$pass) {
-                $fail($messages[$key]);
+                $fail(__('validation.password.'.$key));
             }
         }
     }
