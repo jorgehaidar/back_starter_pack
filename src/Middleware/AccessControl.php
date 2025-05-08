6719f9137855d1456e5ee922f66409518731a148
jorgehaidar <jorgehaidarmtnez@gmail.com>
2025-05-07 23:46:45 -0400

    fix[middleware]: correccion del namespace

--- src/Middleware/AccessControl.php
+++ src/Middleware/AccessControl.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace Mbox\BackCore\Middleware;
 
 use App\Models\Security\User;
 use Closure;
--- src/Middleware/ApiKeyMiddleware.php
+++ src/Middleware/ApiKeyMiddleware.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace Mbox\BackCore\Middleware;
 
 use App\Models\Security\ApiKey;
 use Closure;
